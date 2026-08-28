0925e07b506d64271389979089f5941cd7914104
bohao12 <pengbohao123@gmail.com>
2026-08-28 15:58:25 +0800

    implement Mark and Umark task

--- src/main/java/Yao.java
+++ src/main/java/Yao.java
@@ -17,7 +17,7 @@ public class Yao {
         System.out.println(line);
 
         Scanner scanner = new Scanner(System.in);
-        String[] tasks = new String[100];
+        Task[] tasks = new Task[100];
         int taskCount = 0;
 
         while (true) {
@@ -28,12 +28,25 @@ public class Yao {
                 System.out.println(line);
                 break;
             } else if (command.equals("list")) {
+                System.out.println("Here are the tasks in your list:");
                 for (int i = 0; i < taskCount; i++) {
-                    System.out.println((i + 1) + ". " + tasks[i]);
+                    System.out.println((i + 1) + "." + tasks[i]);
                 }
                 System.out.println(line);
+            } else if (command.startsWith("mark ")) {
+                int taskIndex = Integer.parseInt(command.substring(5).trim()) - 1;
+                tasks[taskIndex].markAsDone();
+                System.out.println("Nice! I've marked this task as done:");
+                System.out.println("  " + tasks[taskIndex]);
+                System.out.println(line);
+            } else if (command.startsWith("unmark ")) {
+                int taskIndex = Integer.parseInt(command.substring(7).trim()) - 1;
+                tasks[taskIndex].markAsUndone();
+                System.out.println("OK, I've marked this task as not done yet:");
+                System.out.println("  " + tasks[taskIndex]);
+                System.out.println(line);
             } else {
-                tasks[taskCount] = command;
+                tasks[taskCount] = new Task(command);
                 taskCount++;
                 System.out.println("added: " + command);
                 System.out.println(line);
