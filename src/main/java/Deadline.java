b2ce849281d45196149dcc0dc9a1b1e24a14fac0
bohao12 <pengbohao123@gmail.com>
2026-09-03 10:43:25 +0800

    Add ToDos, Events, Deadlines

---
+++ src/main/java/Deadline.java
@@ -0,0 +1,22 @@
+/**
+ * Represents a task that needs to be done before a specific date/time.
+ */
+public class Deadline extends Task {
+    protected String by;
+
+    /**
+     * Constructs a Deadline task with description and deadline limit.
+     *
+     * @param description Description of the task.
+     * @param by Due date/time string.
+     */
+    public Deadline(String description, String by) {
+        super(description);
+        this.by = by;
+    }
+
+    @Override
+    public String toString() {
+        return "[D]" + super.toString() + " (by: " + by + ")";
+    }
+}
